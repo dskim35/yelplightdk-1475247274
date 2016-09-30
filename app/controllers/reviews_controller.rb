6bc85f413c82ea90029cc63fb06b8638e377f206
Raghu Betina <raghu@firstdraft.com>
2016-09-30 14:59:41 +0000

    Generate reviews resource with starter:resource

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new
+    @review.review = params[:review]
+    @review.user_id = params[:user_id]
+    @review.restaurant_id = params[:restaurant_id]
+
+    if @review.save
+      redirect_to "/reviews", :notice => "Review created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    @review.review = params[:review]
+    @review.user_id = params[:user_id]
+    @review.restaurant_id = params[:restaurant_id]
+
+    if @review.save
+      redirect_to "/reviews", :notice => "Review updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+
+    @review.destroy
+
+    redirect_to "/reviews", :notice => "Review deleted."
+  end
+end
