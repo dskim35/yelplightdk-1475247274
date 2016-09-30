ebfba4ba27418c6a206f3585e592c864c999dca0
Raghu Betina <raghu@firstdraft.com>
2016-09-30 15:00:24 +0000

    Direct association between tags and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :class_name => "Tag"
+
   belongs_to :review
 
   belongs_to :restaurant
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,10 @@
 class Tag < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :foreign_key => "user_id",
+             :dependent => :destroy
+
   belongs_to :restaurant
 
   # Indirect associations
