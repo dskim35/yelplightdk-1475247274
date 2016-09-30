f00ae4f653466121ddb631ef7dacc47e9682514c
Raghu Betina <raghu@firstdraft.com>
2016-09-30 15:00:06 +0000

    Generate owners resource with starter:resource

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,48 @@
+class OwnersController < ApplicationController
+  def index
+    @owners = Owner.all
+  end
+
+  def show
+    @owner = Owner.find(params[:id])
+  end
+
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    @owner = Owner.new
+    @owner.name = params[:name]
+
+    if @owner.save
+      redirect_to "/owners", :notice => "Owner created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @owner = Owner.find(params[:id])
+  end
+
+  def update
+    @owner = Owner.find(params[:id])
+
+    @owner.name = params[:name]
+
+    if @owner.save
+      redirect_to "/owners", :notice => "Owner updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @owner = Owner.find(params[:id])
+
+    @owner.destroy
+
+    redirect_to "/owners", :notice => "Owner deleted."
+  end
+end
